8b81014558904f2ddce500e4f2b7ecf3f57c64e8
Peter K <peterk@noplace.com>
2017-05-07 14:30:59 +0100

    fix rounding error by specifying width with decimail point (thus making it a float instead of an integer).

--- gen.rb
+++ gen.rb
@@ -1,26 +1,29 @@
+#check out the readme, yo.
+
 require 'victor'
 
 #Robin's palette
 colors = %w"#1B1725 #B8C5D6 #503D3F #481620 #324A5F"
 
 background = "#fff"
-diam = 20
-spacing = diam #spacing equal to diameter
+width = 5000.00
 
 #change these depending on aspect ratio (e.g. square, or 2:1 rectangle)
-rows = 10
 cols = 20
+rows = 10
 
 #how many options do you want? (default: 3)
 how_many = 3
 
+
 #-----careful down here....
+height = width * rows / cols
+spacing_count = cols - 1
+diam = width / (cols + spacing_count) #works because space = diameter
+spacing = diam #spacing equal to diameter
 rad = diam/2
-width = (cols * diam) + ((cols-1) * diam)
-height = (rows * diam) + ((rows-1) * diam)
 
-
-#make 3 versions to choose from
+#make several versions to choose from
 how_many.times do |i|
   
   #initialis a new SVG instance
@@ -28,6 +31,7 @@ how_many.times do |i|
 
   #start filling it with shapes
   svg.build do   
+    
     #solid background 
     rect x: 0, y: 0, width: width, height: height, fill: background
     
@@ -36,10 +40,9 @@ how_many.times do |i|
       #loop through the columns
       cols.times do |col|
         #center X is single radius, + current column * (circle diam + circle spacing)
-        x = rad + (col * (diam+spacing))
+        x = rad + (col * (diam + spacing))
         #ditto for center Y
-        y = rad + (row * (diam+spacing))
-        
+        y = rad + (row * (diam + spacing))
         #now create the circle using a random color from our palette
         circle cx: x, cy: y, r: rad, fill: colors.sample
       end
